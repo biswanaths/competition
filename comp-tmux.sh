0163418bce271ba8f158ccf38dd6f48b7e9655bb
biswanath <biswanaths@gmail.com>
2015-04-01 12:57:21 +0530

    parmeterized session

--- comp-tmux.sh
+++ comp-tmux.sh
@@ -3,11 +3,11 @@ tmux new-session -d
 tmux split-window -h 
 tmux split-window -v 
 tmux select-pane -t 0
-tmux send-keys 'vim a.cpp' Enter
+tmux send-keys 'vim '"$1"'.cpp' Enter
 tmux select-pane -t 1
 tmux send-keys 'vim test.in' Enter
 tmux select-pane -t 2
-tmux send-keys 'sh wr.sh a' Enter
+tmux send-keys 'sh wr.sh '"$1"'' Enter
 tmux select-pane -t 0
 tmux -2 attach-session -d 
 
