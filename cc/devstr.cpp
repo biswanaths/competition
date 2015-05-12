691a8a91ee9fb5e6e09227ebd04d3082738d6336
biswanath <biswanaths@gmail.com>
2015-05-12 13:43:04 +0530

    trying 3

---
+++ cc/devstr.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> PII;
+
+#define lld long long int 
+#define ulld unsigned long long int 
+#define EOL '\0'
+#define PEL cout<<endl;
+#define N 200002
+#define rep(n) for(int i =0;(i)<(int)(n);(i)++)
+#define repij(n,m) for(int i =0;(i)<(int)(n);(i)++) for(int j =0;(j)<(int)(m);(j)++)
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >( \
+                  std::ostringstream() << std::dec << x ).str()
+
+#define For(iterable) for(__typeof__((iterable).begin()) it = (iterable).begin(); it != (iterable).end(); ++it)
+
+string solve(string s,int n, int k) {
+    string ss = "p" + s + "p";
+    char prev = 'p';
+    return s;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+	freopen("test.in", "r",stdin);
+	//freopen("test.out", "w",stdout);
+#endif
+    int t; cin>>t;
+    while(t--) { 
+        int n,k; cin>>n>>k;
+        string s; cin>>s;
+        cout<<solve(s,n,k)<<endl;
+    }
+
+    return 0;
+}
+
