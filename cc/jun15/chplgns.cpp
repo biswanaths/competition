964fd452f213af1e05e1448d8b4954791fd00c69
biswanath <biswanaths@gmail.com>
2015-06-09 11:44:46 +0530

    merge sort does the trick

--- cc/jun15/chplgns.cpp
+++ cc/jun15/chplgns.cpp
@@ -37,6 +37,8 @@ typedef pair<int, int> PII;
 
 #define For(iterable) for(__typeof__((iterable).begin()) it = (iterable).begin(); it != (iterable).end(); ++it)
 
+vector<   vector < PII >   > polys;
+
 bool isin(double x,double y,vector<PII>& poly) {
     int i,j, c=0,sz =poly.size();
     double xi,yi,xj,yj;
@@ -49,6 +51,37 @@ bool isin(double x,double y,vector<PII>& poly) {
     return c;
 }
 
+vector<int> merge(vector<int> left,vector<int> right) {
+    vector<int> merged;
+    int i=0,j=0;
+    for(;i<left.size() && j<right.size();) {
+        int x = polys[left[i]][0].first,y =polys[left[i]][0].second;
+        if(isin(x,y,polys[right[j]]))
+        //if(left[i] <= right[j]) 
+            merged.push_back(left[i++]);
+        else 
+            merged.push_back(right[j++]);
+    }
+
+    while(i<left.size()) merged.push_back(left[i++]);
+    while(j<right.size()) merged.push_back(right[j++]);
+    return merged;
+}
+
+
+vector<int> ms(vector<int> list) {
+    if(list.size() <= 1)
+        return list;
+    vector<int> left, right;
+    int m = list.size()/2;
+    for(int i=0;i<m;i++) left.push_back(list[i]);
+    for(int i=m;i<list.size();i++) right.push_back(list[i]);
+    left = ms(left);
+    right = ms(right);
+    list = merge(left,right);
+    return list;
+}
+
 int main() 
 {
     ios::sync_with_stdio(false);
@@ -59,32 +92,33 @@ int main()
     int t; cin>>t;
     while(t--) {
         int n; cin>>n;
-        vector<   vector < PII >  * > polys;
+        polys.resize(n);
         for(int i=0;i<n;i++) {
             int m; cin>>m;
-            vector< PII> poly(m);
+            polys[i].resize(m);
             for(int j=0;j<m;j++) {
                 int a,b;cin>>a>>b;
-                poly[j].first=a, poly[j].second=b;
+                polys[i][j].first=a, polys[i][j].second=b;
             }
-            polys.push_back(&poly);
         }
         vector<int> ord;
-        rep(polys.size()) ord.push_back(0);
-
+        rep(polys.size()) ord.push_back(i);
+        ord = ms(ord);
+/*
         for(int i=0;i<polys.size();i++) {
-            vector<PII> poly = * (polys[i]);
-            int x = poly[0].first, y = (*polys[i])[0].second;
-            cout<<x<<" "<<y<<endl;
+            int x = polys[i][0].first, y = polys[i][0].second;
             for(int j=i+1;j<polys.size();j++) {
-                if(isin(x,y,(*polys[j]))) 
+                if(isin(x,y,polys[j])) 
                     ord[j]++;
                 else 
                     ord[i]++;
             }
-        }
+        } */
+        vector<int> less(ord.size());
         rep(ord.size()) 
-            cout<<ord[i]<<" " ; PEL;
+            less[ord[i]]=i;
+        rep(less.size())
+            cout<<less[i]<<" " ; PEL;
     }
     return 0;
 }
