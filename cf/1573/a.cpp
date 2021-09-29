887930650dc0754f212e570d6c4917a17daeb93b
biswanath <biswanath.senapati@cuemath.com>
2021-09-29 22:03:26 +0530

    1573

---
+++ cf/1573/a.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> PII;
+
+#define N 100002
+#define rep(n) for(int i =0;(i)<(int)(n);(i)++)
+#define repij(n,m) for(int i =0;(i)<(int)(n);(i)++) for(int j =0;(j)<(int)(m);(j)++)
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >( \
+                  std::ostringstream() << std::dec << x ).str()
+
+#define For(iterable) for(__typeof__((iterable).begin()) it = (iterable).begin(); it != (iterable).end(); ++it)
+
+int solve() {
+    string  c;int n;
+    int ans = 0;
+    cin>>n>>c;
+    for(int i=0;i<n;i++) { 
+        if(c[i]!='0') {
+            ans += (c[i]- '0');
+            ans++;
+        }
+    }
+    if(c[n-1]!='0') ans--;
+    return ans;
+}
+
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+	freopen("test.in", "r",stdin);
+	//freopen("test.out", "w",stdout);
+#endif
+    int t; cin>>t; 
+    while(t--) {
+        cout<<solve()<<endl;
+    }
+    return 0;
+}
+
---
+++ cf/1573/comp-tmux.sh
@@ -0,0 +1,1 @@
+/Users/biswanathsenapati/code/competition/comp-tmux.sh
