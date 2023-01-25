5e03ad768325e30c20b1d5cd6c903daee2219e0e
Biswanath Senapati <biswanathsenapati@CL-BLR-OE-LT-656.local>
2023-01-25 13:06:40 +0530

    1777

---
+++ cf/1777/a.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> PII;
+
+#define N 100002
+#define rep(n) for(int i =0;(i)<(int)(n);(i)++)
+#define repij(n,m) for(int i =0;(i)<(int)(n);(i)++) for(int j =0;(j)<(int)(m);(j)++)
+
+#define SSTR( x ) dynamic_cast< std::ostringstream & >( \
+                  std::ostringstream() << std::dec << x ).str()
+
+#define For(iterable) for(__typeof__((iterable).begin()) it = (iterable).begin(); it != (iterable).end(); ++it)
+
+
+int solve() { 
+    int ans = 0;
+    int l; cin>>l;
+    int a[l+1];
+    for(int i=1;i<=l;i++) { 
+        cin>>a[i];
+    }
+    a[0]=a[1]+1;
+    for(int i=1;i<=l;i++) {
+        if(a[i]%2==a[i-1]%2)
+            ans++;
+    }
+    return ans;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+	freopen("test.in", "r",stdin);
+	//freopen("test.out", "w",stdout);
+#endif
+    int t; cin>>t;
+    while(t--) {
+        cout<<solve()<<endl;
+    }
+
+    return 0;
+}
+
